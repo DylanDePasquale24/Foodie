c4479e4fb26302f8ce05abc4cfabb08873643704
richard <richardqiancs@gmail.com>
2023-01-29 13:56:51 -0500

    Learning datatypes in TS

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Foodie
 ### Description 
 This branch follows an online Angular tutorial from freecodecamp. DO NOT do a 'git pull' to pull changes from main.
-Video: https://www.youtube.com/watch?v=3qBXWUpoPHo 
+Video: https://www.youtube.com/watch?v=3qBXWUpoPHo
 ### Members
 **Front-End**
  Dylan DePasquale and Richard Qian  
--- datatypes.ts
+++ datatypes.ts
@@ -1,3 +1,62 @@
-let lname = 'John';
-
+let lname : string;
+lname = "Qian";
 // lname = 10;
+let newname = lname.toUpperCase();
+
+console.log(newname);
+
+let age: number;
+age = 25;
+age = 25.5;
+let dob = "25";
+let result = parseInt(dob);
+
+let isValid: boolean = true;
+console.log(isValid);
+
+let empList : string[];
+
+empList = ["Qian1", "Qian2", "Qian3"];
+
+let numList : Array<number>;
+
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let results = numList.filter((num) => num > 2);
+
+let num = numList.find((num) => num === 2);
+
+let emp = empList.find((emp) => emp === "Qian1");
+
+let sum = numList.reduce((acc, num) => acc + num);
+
+console.log(results);
+console.log(num);
+console.log(emp);
+console.log(sum);
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let c: Color = Color.Blue;
+
+// Tuples are useful for functions that return multiple values/types
+let swapNums : [number, number];
+
+function swapNumbers(num1: number, num2: number) : [number, number] {
+    return [num2, num1];
+}
+
+swapNums = swapNumbers(10, 20);
+
+swapNums[0];
+swapNums[1];
+
+let department: any;
+
+department = "IT";
+department = 10;
+
