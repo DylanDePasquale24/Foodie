cf34c31df446fe2e36119f1b787f300962854bd9
paulj003 <pauljablonowski@ufl.edu>
2023-02-03 20:10:50 -0500

    Fixed spelling and added two comments

--- backend-boilerplate/app.go
+++ backend-boilerplate/app.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"database/sql"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorrila/mux"
+	"github.com/gorilla/mux"
 )
 
 type App struct {
@@ -12,6 +11,8 @@ type App struct {
 	DB     *sql.DB
 }
 
+// Creates database connection and wires up the routes
 func (a *App) Initialize(usern, pass, loginInfo string) {}
 
+// Starts the application
 func (a *App) Run(addr string) {}
