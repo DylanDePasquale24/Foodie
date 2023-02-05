92731a92f74ee7400467ff123d035d0260aedfc9
Richard Qian <richardqiancs@gmail.com>
2023-02-04 20:15:52 -0500

    Add routing and component functionality with routing

--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent {
   showAddTask: boolean = false;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe(
       (value) => this.showAddTask = value);
   }
@@ -21,5 +22,9 @@ export class HeaderComponent {
 
   toggleAddTask() {
     this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
