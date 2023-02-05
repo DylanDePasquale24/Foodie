12cc2d63a2814e7c73826a25b4ae40f45a64da3f
Richard Qian <richardqiancs@gmail.com>
2023-02-04 20:05:24 -0500

    Complete CRUD Project from Tutorial

---
+++ angular-crash/src/app/services/ui.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Subject, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiService {
+  private showAddTask: boolean = false;
+  private subject = new Subject<any>();
+
+  constructor() { }
+
+  toggleAddTask(): void {
+    this.showAddTask = !this.showAddTask;
+    this.subject.next(this.showAddTask);
+  }
+
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
+  }
+}
