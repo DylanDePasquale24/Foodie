7c0a5aad494daa1acfb58611da5f84d0647a66cf
Richard Qian <richardqiancs@gmail.com>
2023-02-05 19:54:56 -0500

    Add HTTP Client module

--- Angular Frontend/src/app/app.module.ts
+++ Angular Frontend/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { AuthGuard } from './auth.guard';
+// import { JwtInterceptor } from './jwt.interceptor';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -20,12 +23,15 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     FormsModule
   ],
   exports: [RouterModule],
-  providers: [],
+  providers: [
+    // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
