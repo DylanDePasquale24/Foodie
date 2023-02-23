e7802b6d516925b69ff9d266e4e214995d5ba572
Richard Qian <richardqiancs@gmail.com>
2023-02-23 12:40:44 -0500

    Import necessary modules for login test

--- Angular Frontend/src/app/login/login.component.spec.ts
+++ Angular Frontend/src/app/login/login.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatFormField } from '@angular/material/form-field';
+import { MatLabel } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginComponent } from './login.component';
+import { LoginToolbarComponent } from '../login-toolbar/login-toolbar.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +17,20 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports : [
+        HttpClientTestingModule,
+        FormsModule, 
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [ 
+        LoginComponent,  
+        LoginToolbarComponent,
+        MatToolbar,
+        MatFormField,
+        MatLabel
+      ]
     })
     .compileComponents();
 
