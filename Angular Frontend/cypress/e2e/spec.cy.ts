70c093d1ac0486b453a271e8f7fab35a4895c9aa
Richard Qian <richardqiancs@gmail.com>
2023-02-27 18:29:48 -0500

    Cypress tests for landing page

--- Angular Frontend/cypress/e2e/spec.cy.ts
+++ Angular Frontend/cypress/e2e/spec.cy.ts
@@ -1,6 +1,22 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('')
-    cy.contains('Recipe tracking made easy')
+describe('Goes to Landing Page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  
+  it('Visits landing page and checks main elements', () => {
+    cy.contains(/foodie/i)
+    cy.contains('button', /log in/i)
+    cy.contains('button', /start for free/i)
+    cy.contains(/recipe tracking made easy/i)
   })
+
+  it('Should have the login toolbar', () => {
+    cy.get('app-login-toolbar').should('exist')
+    cy.get('app-login-toolbar').should('be.visible')
+  });
+
+  it('Should navigate to register page when clicking Start for Free button', () => {
+    cy.get('#mainInfo button').click();
+    cy.url().should('include', '/register');
+  });
 })
