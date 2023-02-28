5e851662ebe8bc6b632789d062488b0097082449
paulj003 <pauljablonowski@ufl.edu>
2023-02-28 16:16:53 -0500

    Added test to check if registering a user works

--- Backend/main_test.go
+++ Backend/main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"testing"
-
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,4 +24,28 @@ func TestHashPassword(t *testing.T) {
 	if error != nil {
 		t.Errorf("Error in Hashing, Error Message: %d", error)
 	}
+}
+
+/* Test tries to register a user using the info in the json variable
+	Returns code 200 when passes
+*/
+func TestRouterPOSTRegister(t *testing.T) {
+	router := setupRouter()
+
+	RouterPOSTRegister(router)
+
+	responseRecorder := httptest.NewRecorder()
+
+	json := []byte(`{
+		"firstName": "testing",
+		"lastName": "testing",
+		"email": "paul@gmail.com",
+		"password": "paul1234"}`)
+
+	request, _ := http.NewRequest("POST", "/register", bytes.NewBuffer(json))
+
+	router.ServeHTTP(responseRecorder, request)
+
+	assert.Equal(t, 200, responseRecorder.Code, "Error Message: %s", responseRecorder.Body.String())
+	
 }
