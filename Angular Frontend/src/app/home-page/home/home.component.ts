398379a2afd0277a005446b9950586471fa36c36
Dylan DePasquale <ddepasquale@ufl.edu>
2023-04-05 14:13:31 -0400

    made interfaces and planned more

--- Angular Frontend/src/app/home-page/home/home.component.ts
+++ Angular Frontend/src/app/home-page/home/home.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog'  //service
 import { AddRecipeDialogComponent } from '../add-recipe-dialog/add-recipe-dialog.component';
+import { HttpClient } from '@angular/common/http';
+import { uniq } from 'cypress/types/lodash';
 
+
+interface Macros {
+  calories : number
+  carbs : number
+  protein : number
+  fat : number
+}
+interface Recipe {
+  name : string
+  description : string
+  ingredients : Array<string>
+  ingredientMacros : Array<Macros>   //index corresponds to index of the ingredients array
+  instructions : string
+
+  //totalMacros : Macros   //for the entire recipe
+}
+/*COULD HAVE A RESPONSE RECIPE AND A FRONTEND RECIPE TOO (SEPERATE) */
+
+//could also do a pair -> ingredients : Array<string, Macros>  
+  //can backend do?
 
 @Component({
   selector: 'app-home',
@@ -25,8 +47,11 @@ export class HomeComponent {
     maxWidth : string,
     data : {userID: string | null}
   }
+
+  //Array stores all of recipes
+  recipes : Array<Recipe>
   
-  constructor(private dialogService: MatDialog){
+  constructor(private httpClient: HttpClient,private dialogService: MatDialog){
 
     this.user = {
       id: localStorage.getItem('userId'),
@@ -44,6 +69,8 @@ export class HomeComponent {
       data: { userID: this.user.id} 
     }
 
+    this.recipes = []  //for now... may need to populate it with the GetRecipes() function somehow
+
     //in the constructor do a call to the http to fetch recipes and store in the recipes array
     //or maybe theres a way to constantly refresh... (websocket i think?)
     //or cud just do in constructor, and everytime the user adds a new recipe
@@ -59,16 +86,20 @@ export class HomeComponent {
       console.log(result)
     })
   }
+  GetRecipes(){
+
+    this.httpClient
+    .get<Array<Recipe>>('/recipeGet/:' + this.user.id)
+    .subscribe((recipeListResponse : Array<Recipe>) => {
+
+      //What to do with response 
+
+
+    }, (err) =>{
+
+      //What to do when theres an error
+    })
+
+  }
 }
 
-/*==== PLANNING ====*/
-
-//Recipe Object  -> should be an interface instead
-  //name - string
-  //description - string
-  //ingredients - string[] with ingredient and amt seperated by delimiter  "chicken | 250"
-  //instructions - string
-  //ingredientMacros - macroObj[]   (where each nutrient object has calories, fat, protein, carbs)
-    //or null
-
-//Macros
